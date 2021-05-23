8aa0620bfef0a7b8ee310f50074d793f4875e13f
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-05-23 17:35:49 +0200

    Hotfix 2 of the day: update main suggester option names

--- src/constants.ts
+++ src/constants.ts
@@ -8,6 +8,6 @@ export const ADD_TO_END: string = "cmd:end";
 export const newDataView: string = "New Dataview field";
 export const newYaml: string = "New YAML property";
 export const MAIN_SUGGESTER_OPTIONS: Property[] = [
-    {key: "newYaml", content: newYaml, type: MetaType.Option},
-    {key: "newDataView", content: newDataView, type: MetaType.Option}
+    {key: newYaml, content: newYaml, type: MetaType.Option},
+    {key: newDataView, content: newDataView, type: MetaType.Option}
 ]
