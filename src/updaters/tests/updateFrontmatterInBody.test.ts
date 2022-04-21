a0b3ac9ea8ef7c7644635a87b6d584ca3f0c2f14
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-04-21 14:36:56 +0200

    update frontmatter in body test

--- src/updaters/tests/updateFrontmatterInBody.test.ts
+++ src/updaters/tests/updateFrontmatterInBody.test.ts
@@ -1,13 +1,9 @@
-import { stringifyYaml } from 'obsidian';
 import updateFrontmatterInBody from '../updateFrontmatterInBody';
 
 describe('update frontmatter in body string', () => { 
     it('adds frontmatter to empty body', () => {
         const body = '';
-        const newFrontmatter = stringifyYaml({
-            title: 'test',
-            description: 'test',
-        });
+        const newFrontmatter = `title: test\ndescription: test`;
 
         const result = updateFrontmatterInBody(body, newFrontmatter);
         expect(result).toBe(`---\n${newFrontmatter}---\n`);
