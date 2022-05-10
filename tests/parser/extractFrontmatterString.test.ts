12c496192d8632bc8b45668a3cf1a0a2f1f252b8
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-05-10 15:53:39 +0200

    Fix test variable naming semantics

--- tests/parser/extractFrontmatterString.test.ts
+++ tests/parser/extractFrontmatterString.test.ts
@@ -2,20 +2,20 @@ import {extractFrontmatterString} from "../../src/parser/extractFrontmatterStrin
 
 describe("extractFrontmatterString", () => {
   it("should extract frontmatter string", () => {
-    const frontmatterString = `---\ntitle: "Hello World"\n---\n`;
-    const result = extractFrontmatterString(frontmatterString);
-    expect(result).toEqual(`title: "Hello World"`);
+    const body = `---\ntitle: "Hello World"\n---\n`;
+    const frontmatterString = extractFrontmatterString(body);
+    expect(frontmatterString).toEqual(`title: "Hello World"`);
   });
 
     it('should return empty string on empty frontmatter body', function () {
-        const frontmatterString = `---\n---\n`;
-        const result = extractFrontmatterString(frontmatterString);
-        expect(result).toEqual('');
+        const body = `---\n---\n`;
+        const frontmatterString = extractFrontmatterString(body);
+        expect(frontmatterString).toEqual('');
     });
 
     it('should return empty string on no frontmatter', function () {
-        const frontmatterString = `Hello World`;
-        const result = extractFrontmatterString(frontmatterString);
-        expect(result).toEqual('');
+        const body = `Hello World`;
+        const frontmatterString = extractFrontmatterString(body);
+        expect(frontmatterString).toEqual('');
     });
 });
